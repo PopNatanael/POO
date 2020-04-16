98dc83596c3bde452fb4446e0951f2dc197a05b4
Pop Natanael <56395369+PopNatanael@users.noreply.github.com>
2020-04-16 13:17:12 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo info = new DirectoryInfo(@"F:/Filme");
+            long MarDir = SOD(info, true);
+            Console.WriteLine("Marimea Folderului : " +
+            "{0:N0} Bytes", MarDir);
+            
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+        static long SOD(DirectoryInfo info, bool b)
+        {
+            long MarTot = info.EnumerateFiles()
+                         .Sum(file => file.Length);
+            if (b)
+            {
+                MarTot += info.EnumerateDirectories()
+                         .Sum(dir => SOD(dir, true));
+            }
+            return MarTot;
+        }
+    }
+}
+
